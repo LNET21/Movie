1d4d5730af6c57b990eea662b3911cae922ac8af
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-08-17 11:19:31 +0200

    Add project files.

---
+++ Movie/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie
+{
+    public enum Genre
+    {
+        Horror,
+        Thriller,
+        Comedy,
+        Drama,
+        Action
+    }
+}
